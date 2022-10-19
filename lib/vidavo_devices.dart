d5e98aee7626828d8e3fe0355a2cddea296db69f
xtoza <xtozakidis@vidavo.eu>
2022-10-19 10:29:46 +0300

    bluetooth scan

--- lib/vidavo_devices.dart
+++ lib/vidavo_devices.dart
@@ -1,4 +1,32 @@
 library vidavo_devices;
 
-class VidavoDevices {
+import 'dart:async';
+
+import 'package:flutter_blue/flutter_blue.dart';
+
+class VidavoDevices  extends ScanBluetooth {
+
+
+
+
+
+
+}
+
+class ScanBluetooth{
+  late StreamSubscription subscriptionFindAndConnect;
+
+  findDevice()  {
+    FlutterBlue.instance.startScan(timeout: const Duration(minutes: 4));
+    Stream<List<ScanResult>> scanStream = FlutterBlue.instance.scanResults;
+    subscriptionFindAndConnect = scanStream.listen((event) {
+      event.forEach((element)  {
+        if (element.device.name.startsWith("BF600") ) {
+          FlutterBlue.instance.stopScan();
+       print("hello world");
+          subscriptionFindAndConnect.cancel();
+        }
+      });
+    });
+  }
 }
