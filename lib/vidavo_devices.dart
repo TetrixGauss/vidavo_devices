867ca1f274fc55cd9d3b565b0fb715c20f069100
xtoza <xtozakidis@vidavo.eu>
2022-10-20 13:41:14 +0300

    bluetooth scan

--- lib/vidavo_devices.dart
+++ lib/vidavo_devices.dart
@@ -1,7 +1,7 @@
 library vidavo_devices;
 
 import 'package:vidavo_devices/bf600.dart';
-import 'package:vidavo_devices/Wearable.dart';
+import 'package:vidavo_devices/wearable_connection.dart';
 
 
 
